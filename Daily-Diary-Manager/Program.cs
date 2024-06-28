9103623aef691af5f8bd39ad58ce122f712b319d
yazanMhussein <yazan.m.hussein@gmail.com>
2024-06-28 18:12:56 +0300

    Add project files.

---
+++ Daily-Diary-Manager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Daily_Diary_Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
