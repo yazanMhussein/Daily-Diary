5cec8cc43a4c1a78e4d027784a37d741d23bd096
yazanMhussein <yazan.m.hussein@gmail.com>
2024-06-28 18:49:36 +0300

    test

--- Daily-Diary-Manager/DailyDiary.cs
+++ Daily-Diary-Manager/DailyDiary.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using Daily_Diary_Manager;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace Daily_Diary_Manager
+public class DailyDiary
 {
-    internal class DailyDiary
+    private string _filePath;
+
+    public DailyDiary(string filePath)
     {
+        _filePath = filePath;
+    }
+
+    public void ReadDiary()
+    {
+        if (File.Exists(_filePath))
+        {
+            string[] lines = File.ReadAllLines(_filePath);
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Diary file not found.");
+        }
+    }
+
+    public void AddEntry(Entry entry)
+    {
+        using (StreamWriter sw = new StreamWriter(_filePath, true))
+        {
+            sw.WriteLine(entry.ToString() + Environment.NewLine);
+        }
+    }
+
+    public void DeleteEntry(DateTime date)
+    {
+        if (File.Exists(_filePath))
+        {
+            List<string> lines = new List<string>(File.ReadAllLines(_filePath));
+            lines.RemoveAll(line => line.StartsWith(date.ToString("yyyy-MM-dd")));
+            File.WriteAllLines(_filePath, lines);
+        }
+        else
+        {
+            Console.WriteLine("Diary file not found.");
+        }
+    }
+
+    public int CountEntries()
+    {
+        if (File.Exists(_filePath))
+        {
+            string[] lines = File.ReadAllLines(_filePath);
+            return lines.Length;
+        }
+        else
+        {
+            Console.WriteLine("Diary file not found.");
+            return 0;
+        }
+    }
+
+    public void SearchEntries(DateTime date)
+    {
+        if (File.Exists(_filePath))
+        {
+            string[] lines = File.ReadAllLines(_filePath);
+            foreach (string line in lines)
+            {
+                if (line.StartsWith(date.ToString("yyyy-MM-dd")))
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Diary file not found.");
+        }
     }
 }
+ 
